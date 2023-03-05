14515181309f4391f1e927c216c84285ff935049
Nikolay Gelemerov <nikolaygelemerov@gmail.com>
2023-03-05 02:16:56 +0200

    Update `GcloudModel` tests

--- app/services/models/GcloudModel/tests/GcloudModel.test.tsx
+++ app/services/models/GcloudModel/tests/GcloudModel.test.tsx
@@ -1,13 +1,16 @@
 import { describe, expect, it } from 'vitest';
 
-import type { ElectricityUsageOptions } from '~/types';
+import type { ElectricityQueryOptions } from '~/types';
 
 import { GcloudModel } from '../GcloudModel';
 
 describe('GcloudModel', () => {
   it('buildUsageFilePath', () => {
-    const test: { expected: string; input: ElectricityUsageOptions }[] = [
-      { expected: 'prices/2023/02/01.jsonl', input: { day: '01', month: '02', year: '2023' } }
+    const test: { expected: string; input: ElectricityQueryOptions }[] = [
+      {
+        expected: 'usage/2023/02/01/1234.jsonl',
+        input: { day: '01', meteringPointId: '1234', month: '02', year: '2023' }
+      }
     ];
 
     test.forEach(({ expected, input }) =>
